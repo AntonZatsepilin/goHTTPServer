a4f7910877858567f949a32849233c48b12210f1
AntonZatsepilin <zatsepilin90.00@bk.ru>
2024-09-23 11:10:17 +0300

    init

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type User struct {
+	ID   int    `json:"id"`
+	Name string `json:"name,omitempty"`
+}
+
+var users = []User{{1, "Anton"}, {2, "Max"}, {3, "Tim"}}
+
+func main() {
+	http.HandleFunc("/users", handleUsers)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handleUsers(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		getUsers(w, r)
+	case http.MethodPost:
+		addUser(w, r)
+	default:
+		w.WriteHeader(http.StatusNotImplemented)
+	}
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	resp, err := json.Marshal(users)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(resp)
+}
+
+func addUser(w http.ResponseWriter, r *http.Request) {
+	reqBytes, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	var user User
+	err = json.Unmarshal(reqBytes, &user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	users = append(users, user)
+}
